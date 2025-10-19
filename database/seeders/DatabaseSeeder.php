edf239a21dc43e097c887ab72eee0d95847f0a3b
rafliakmalg <apaajapcture@gmail.com>
2025-10-19 17:52:56 +0700

    fix minor on seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,7 +13,7 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         $this->call([
-            PenjualanSeeder::class,
+            PelangganSeeder::class,
             BarangSeeder::class,
             PenjualanSeeder::class,
         ]);
